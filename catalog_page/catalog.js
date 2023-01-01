50ddca2304d81e23b46697882f36c1d0049bd59f
Setvioss <setvios@gmail.com>
2023-01-01 12:35:13 +0200

    catalog filtering logic added

--- catalog_page/catalog.js
+++ catalog_page/catalog.js
@@ -2,7 +2,8 @@ const data = [
     { 
         id: 3455,
         price: 34000,
-        size: '220x90'
+        size: '220x90',
+        color: 'green'
     },
 
     { 
@@ -13,15 +14,65 @@ const data = [
     { 
         id: 3578,
         price: 56000,
+        size: '240x90',
+        color: 'red'
+    },
+    { 
+        id: 3577,
+        price: 48000,
+        size: '240x90',
+        color: 'green'
+    },
+    { 
+        id: 3633,
+        price: 15000,
         size: '240x90'
+    },
+    { 
+        id: 3678,
+        price: 23000,
+        size: '240x90',
+        color: 'green'
+    },
+    { 
+        id: 3698,
+        price: 84000,
+        size: '240x90',
+        color: 'red'
+    },
+    { 
+        id: 3767,
+        price: 41000,
+        size: '240x90'
+    },
+    { 
+        id: 3845,
+        price: 27000,
+        size: '240x90',
+        color: 'red'
+    },
+    { 
+        id: 3814,
+        price: 19000,
+        size: '240x90',
+        color: 'red'
     }
 
 ]
 
+let filtered_data_14_25;
+let filtered_data_25_40;
+let filtered_data_40_60;
+let filtered_data_60;
+
+let current_data;
+
+
 const select = document.querySelector('.sort')
 const card = document.querySelector('.product-cards-block')
+const filter_buttons = document.querySelectorAll('.filter-button')
 
-const display_data = (data) => {
+const display_data__func = (data) => {
 
     card.innerHTML = "";
 
@@ -55,19 +106,180 @@ const display_data = (data) => {
     
     })
 
+    current_data = data;
 }
 
-display_data(data);
+data.sort((a, b) => {
+    return a.price - b.price;
+})
 
+display_data__func(data);
+
+
+
+const price_filter__func = (filter_query) => {
+
+    console.log(filter_query)
+
+    if (filter_query === 'ВСІ') {
+
+        display_data__func(data);
+
+        select.addEventListener('change', e => {
+            if (e.target.value === "1") {
+
+                data.sort((a, b) => {
+                    return a.price - b.price;
+                })
+
+                display_data__func(data);
+
+            } else {
+
+                data.sort((a, b) => {
+                    return b.price - a.price;
+                })
+
+                display_data__func(data);
+            }
+        })
+
+
+    } else if (filter_query === 'ВІД 14 000 ДО 25 000') {
+
+        filtered_data_14_25 = data.filter(item => {
+
+            return item.price >= 14000 && item.price <= 25000;
+
+        })
+
+        select.addEventListener('change', e => {
+            if (e.target.value === "1") {
+
+                filtered_data_14_25.sort((a, b) => {
+                    return a.price - b.price;
+                })
+
+                display_data__func(filtered_data_14_25);
+
+            } else {
+
+                filtered_data_14_25.sort((a, b) => {
+                    return b.price - a.price;
+                })
+
+                display_data__func(filtered_data_14_25);
+            }
+        })
+
+        display_data__func(filtered_data_14_25);
+
+        console.dir(card)
+
+
+    } else if (filter_query === 'ВІД 25 000 ДО 40 000') {
+
+        filtered_data_25_40 = data.filter(item => {
+
+            return item.price >= 25000 && item.price <= 40000;
+
+        })
+
+        select.addEventListener('change', e => {
+            if (e.target.value === "1") {
+
+                filtered_data_25_40.sort((a, b) => {
+                    return a.price - b.price;
+                })
+
+                display_data__func(filtered_data_25_40);
+
+            } else {
+
+                filtered_data_25_40.sort((a, b) => {
+                    return b.price - a.price;
+                })
+
+                display_data__func(filtered_data_25_40);
+            }
+        })
+
+        display_data__func(filtered_data_25_40);
+
+
+    } else if (filter_query === 'ВІД 40 000 ДО 60 000') {
+
+        filtered_data_40_60 = data.filter(item => {
+
+            return item.price >= 40000 && item.price <= 60000;
+
+        })
+
+        select.addEventListener('change', e => {
+            if (e.target.value === "1") {
+
+                filtered_data_40_60.sort((a, b) => {
+                    return a.price - b.price;
+                })
+
+                display_data__func(filtered_data_40_60);
+
+            } else {
+
+                filtered_data_40_60.sort((a, b) => {
+                    return b.price - a.price;
+                })
+
+                display_data__func(filtered_data_40_60);
+            }
+        })
+
+        display_data__func(filtered_data_40_60);
+
+        
+    } else if (filter_query === 'ВІД 60 000') {
+
+        filtered_data_60 = data.filter(item => {
+
+            return item.price >= 60000;
+
+        })
+
+        select.addEventListener('change', e => {
+            if (e.target.value === "1") {
+
+                filtered_data_60.sort((a, b) => {
+                    return a.price - b.price;
+                })
+
+                display_data__func(filtered_data_60);
+
+            } else {
+
+                filtered_data_60.sort((a, b) => {
+                    return b.price - a.price;
+                })
+
+                display_data__func(filtered_data_60);
+            }
+        })
+
+        display_data__func(filtered_data_60);
+
+        
+    }
+
+    
+}
 
 select.addEventListener('change', e => {
     if (e.target.value ==="1"){
 
-        data.sort((a, b) => {
+        data.sort((a, b) => {   
             return a.price - b.price;
         })
 
-        display_data(data);
+        display_data__func(data);
 
     } else {
 
@@ -75,9 +287,18 @@ select.addEventListener('change', e => {
             return b.price - a.price;
         })
 
-        display_data(data);
+        display_data__func(data);
     }
 })
 
+filter_buttons.forEach(item => {
 
+    item.addEventListener('click', e => {
+
+        let filter_query = e.target.innerText;
+
+        price_filter__func(filter_query)
+
+    })
+})
 
