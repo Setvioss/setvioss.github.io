541ded313a75b3341768a081b65d235fff2c64fb
Setvioss <setvios@gmail.com>
2023-01-11 20:45:43 +0200

    price and color filtering logic improved

--- catalog_page/catalog.js
+++ catalog_page/catalog.js
@@ -65,7 +65,11 @@ let filtered_data_25_40 = [];
 let filtered_data_40_60 = [];
 let filtered_data_60 = [];
 
+let filtered_color_data_red = [];
+let filtered_color_data_green = [];
+
 let filtered_price_data = [];
+let filtered_color_data = [];
 
 let current_data;
 
@@ -75,7 +79,7 @@ const card = document.querySelector('.product-cards-block')
 
 
 const display_data__func = (data) => {
-
+    
     card.innerHTML = "";
 
     data.forEach(item => {
@@ -98,6 +102,13 @@ const display_data__func = (data) => {
     
             <p>Розмір</p>
             <p>${item.size}</p>
+    
+        </div>
+
+        <div class="product-card_info-row">
+    
+            <p>Колір</p>
+            <p>${item.color}</p>
     
         </div>
     
@@ -136,16 +147,17 @@ data.sort((a, b) => {
 
 display_data__func(data);
 
+
 const filter_buttons = document.querySelectorAll('.price-filter-content .filter-button')
 const all_prices_button = document.querySelector('[data-low-price="0"]')
 
+const color_filter_buttons = document.querySelectorAll('.color-filter-button')
+const all_color_filter_button = document.querySelector('[data-first="all_button"]')
+
+
 const price_filter__func = (filter_query, e) => {
 
-    console.log(filter_query)
-
-    console.log(filtered_price_data)
     filtered_price_data = []
-    console.log(filtered_price_data)
 
     if (filter_query === 'ВСІ' && e.classList.contains('active')){
 
@@ -158,9 +170,21 @@ const price_filter__func = (filter_query, e) => {
 
     }    
 
-    if (filter_query === 'ВІД 14 000 ДО 25 000' && e.classList.contains('active')){
+    if (filter_query === 'ВІД 14 000 ДО 25 000' && 
+        e.classList.contains('active') &&
+        all_color_filter_button.classList.contains('active')){
 
         filtered_data_14_25 = data.filter(item => {
+
+            return item.price >= 14000 && item.price <= 25000;
+
+        })
+
+    } else if (filter_query === 'ВІД 14 000 ДО 25 000' && 
+                e.classList.contains('active') &&
+                !all_color_filter_button.classList.contains('active')){
+
+                filtered_data_14_25 = filtered_color_data.filter(item => {
 
             return item.price >= 14000 && item.price <= 25000;
 
@@ -180,6 +204,16 @@ const price_filter__func = (filter_query, e) => {
 
         })
 
+    } else if (filter_query === 'ВІД 25 000 ДО 40 000' &&
+                e.classList.contains('active') &&
+                !all_color_filter_button.classList.contains('active')){
+
+                filtered_data_25_40 = filtered_color_data.filter(item => {
+
+                    return item.price >= 25000 && item.price <= 40000;
+            
+                })
+
     } else if (filter_query === 'ВІД 25 000 ДО 40 000' && !e.classList.contains('active')){
 
         filtered_data_25_40 = [];
@@ -193,6 +227,16 @@ const price_filter__func = (filter_query, e) => {
             return item.price >= 40000 && item.price <= 60000;
 
         })
+
+    } else if (filter_query === 'ВІД 40 000 ДО 60 000' &&
+                e.classList.contains('active') &&
+                !all_color_filter_button.classList.contains('active')){
+
+                filtered_data_40_60 = filtered_color_data.filter(item => {
+
+                    return item.price >= 40000 && item.price <= 60000;
+            
+                })
 
     } else if (filter_query === 'ВІД 40 000 ДО 60 000' && !e.classList.contains('active')){
 
@@ -208,16 +252,21 @@ const price_filter__func = (filter_query, e) => {
 
         })
 
+    } else if (filter_query === 'ВІД 60 000' &&
+                e.classList.contains('active') &&
+                !all_color_filter_button.classList.contains('active')){
+
+                filtered_data_60 = filtered_color_data.filter(item => {
+
+                    return item.price >= 60000;
+            
+                })
+
     } else if (filter_query === 'ВІД 60 000' && !e.classList.contains('active')){
 
         filtered_data_60 = [];
 
     }
-
-    console.log(filtered_data_14_25)
-    console.log(filtered_data_25_40)
-    console.log(filtered_data_40_60)
-    console.log(filtered_data_60)
 
     filtered_price_data = filtered_price_data.concat(
         filtered_data_14_25, 
@@ -243,7 +292,28 @@ const price_filter__func = (filter_query, e) => {
 
     if (filter_query !== 'ВСІ') display_data__func(filtered_price_data)
 
-    //console.log(filter_buttons.every(each => classList.contains('active')))
+    if (filter_query !== 'ВСІ' &&
+        !filter_buttons[1].classList.contains('active') &&
+        !filter_buttons[2].classList.contains('active') &&
+        !filter_buttons[3].classList.contains('active') &&
+        !filter_buttons[4].classList.contains('active')){
+
+        display_data__func(filtered_color_data)
+
+    }
+
+    if (filter_query !== 'ВСІ' &&
+        !color_filter_buttons[1].classList.contains('active') &&
+        !color_filter_buttons[2].classList.contains('active') &&
+        !filter_buttons[1].classList.contains('active') &&
+        !filter_buttons[2].classList.contains('active') &&
+        !filter_buttons[3].classList.contains('active') &&
+        !filter_buttons[4].classList.contains('active')){
+
+        display_data__func(data)
+
+    }
+
     if (!filter_buttons[1].classList.contains('active') &&
         !filter_buttons[2].classList.contains('active') &&
         !filter_buttons[3].classList.contains('active') &&
@@ -265,7 +335,7 @@ const price_filter__func = (filter_query, e) => {
     
         }
 
-        display_data__func(data)
+      //  display_data__func(data)
 
     }
 
@@ -298,3 +368,175 @@ filter_buttons.forEach(item => {
     })
 })
 
+
+
+color_filter_buttons.forEach(item => {
+
+    item.addEventListener('click', e => {
+
+        if (e.target !== all_color_filter_button){
+
+            all_color_filter_button.classList.remove('active')
+
+            e.target.classList.toggle('active')
+
+        } else {
+
+            color_filter_buttons.forEach(item => {
+                item.classList.remove('active')
+            }) 
+
+            e.target.classList.add('active')
+        }
+
+        let filter_query = e.target.innerText;
+
+        color_filter__func(filter_query, e.target)
+        
+    })
+})
+
+const color_filter__func = (filter_query, e) => {
+
+
+    filtered_color_data = []
+
+    if (filter_query === 'ВСІ' && 
+        e.classList.contains('active') &&
+        all_prices_button.classList.contains('active')
+        ){
+
+        filtered_color_data_red = [];
+        filtered_color_data_green = [];
+
+        display_data__func(data)
+
+    } 
+
+    if (filter_query === 'ЧЕРВОНІ' && 
+        e.classList.contains('active') &&
+        !all_prices_button.classList.contains('active')
+    
+    ){
+
+        filtered_color_data_red = filtered_price_data.filter(item => {
+
+            return item.color === 'red';
+
+        })
+
+    } else if (filter_query === 'ЧЕРВОНІ' &&
+        e.classList.contains('active') &&
+        all_prices_button.classList.contains('active')){
+
+        filtered_color_data_red = data.filter(item => {
+
+            return item.color === 'red';
+
+        })
+    
+    } else if (filter_query === 'ЧЕРВОНІ' && !e.classList.contains('active')){
+
+        filtered_color_data_red = [];
+
+    }
+
+    if (filter_query === 'ЗЕЛЕНІ' && 
+        e.classList.contains('active') &&
+        !all_prices_button.classList.contains('active')
+    
+    ){
+
+        filtered_color_data_green = filtered_price_data.filter(item => {
+
+            return item.color === 'green';
+
+        })
+
+    } else if (filter_query === 'ЗЕЛЕНІ' &&
+        e.classList.contains('active') &&
+        all_prices_button.classList.contains('active')){
+
+        filtered_color_data_green = data.filter(item => {
+
+            return item.color === 'green';
+
+        })
+    
+    } else if (filter_query === 'ЗЕЛЕНІ' && !e.classList.contains('active')){
+
+        filtered_color_data_green = [];
+
+    }
+
+    filtered_color_data = filtered_color_data.concat(
+        filtered_color_data_red, 
+        filtered_color_data_green,
+    )
+    console.dir(filtered_color_data)
+
+    if (select.value ==='1'){
+
+        filtered_color_data.sort((a, b) => {   
+            return a.price - b.price;
+        })
+
+    } else {
+
+        filtered_color_data.sort((a, b) => {   
+            return b.price - a.price;
+        })
+
+    }
+
+    if (filter_query !== 'ВСІ'){
+        
+        display_data__func(filtered_color_data)
+        console.log('hello')
+    
+    } 
+    
+    if (filter_query !== 'ВСІ' &&
+        !color_filter_buttons[1].classList.contains('active') &&
+        !color_filter_buttons[2].classList.contains('active')){
+
+        display_data__func(filtered_price_data)
+
+    }
+
+    if (filter_query !== 'ВСІ' &&
+        !color_filter_buttons[1].classList.contains('active') &&
+        !color_filter_buttons[2].classList.contains('active') &&
+        !filter_buttons[1].classList.contains('active') &&
+        !filter_buttons[2].classList.contains('active') &&
+        !filter_buttons[3].classList.contains('active') &&
+        !filter_buttons[4].classList.contains('active')){
+
+        display_data__func(data)
+
+    }
+
+    if (!color_filter_buttons[1].classList.contains('active') &&
+        !color_filter_buttons[2].classList.contains('active') 
+    ){
+        all_color_filter_button.classList.add('active')
+
+        if (select.value === '1'){
+
+            data.sort((a, b) => {   
+                return a.price - b.price;
+            })
+    
+        } else {
+    
+            data.sort((a, b) => {   
+                return b.price - a.price;
+            })
+    
+        }
+
+       // display_data__func(data)
+
+    }
+
+}
